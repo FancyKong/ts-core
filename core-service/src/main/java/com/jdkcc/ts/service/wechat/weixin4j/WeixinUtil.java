595bec248fb8a54aa7403e8e5b94649e38b8cc14
Cherish <785427346@qq.com>
2017-02-28 09:18:15 +0800

    WeixinUtil改为使用ObjectMapper 因为用到了Json注解功能

--- core-service/src/main/java/com/jdkcc/ts/service/wechat/weixin4j/WeixinUtil.java
+++ core-service/src/main/java/com/jdkcc/ts/service/wechat/weixin4j/WeixinUtil.java
@@ -1,6 +1,7 @@
 package com.jdkcc.ts.service.wechat.weixin4j;
 
-import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpEntity;
@@ -12,6 +13,13 @@ import org.apache.http.util.EntityUtils;
 
 @Slf4j
 public class WeixinUtil {
+
+	private static final ObjectMapper mapper;
+	static {
+		mapper = new ObjectMapper();
+		// 设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+	}
 	
 	/**
 	 * 通过授权的code取得openid
@@ -29,7 +37,7 @@ public class WeixinUtil {
 		try {
 			CloseableHttpResponse response = httpClient.execute(httpGet);
 			HttpEntity entity = response.getEntity();
-			oAuthInfo = JSON.parseObject(EntityUtils.toString(entity,"UTF-8"),
+			oAuthInfo = mapper.readValue(EntityUtils.toString(entity,"UTF-8"),
 					OAuthInfo.class);
 		} catch (Exception e) {
             log.error("getOAuthOpenid Fail ",Throwables.getStackTraceAsString(e));
@@ -46,7 +54,7 @@ public class WeixinUtil {
 		try {
 			CloseableHttpResponse response = httpClient.execute(httpGet);
 			HttpEntity entity = response.getEntity();
-			userInfo = JSON.parseObject(EntityUtils.toString(entity,"UTF-8"),
+			userInfo = mapper.readValue(EntityUtils.toString(entity,"UTF-8"),
 					UserInfo.class);
 		} catch (Exception e) {
             log.error("getUserInfo Fail ",Throwables.getStackTraceAsString(e));
